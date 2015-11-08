220ec4f546d24b85f4f953419950aa9ffcbaf18e
Darkerego <darkerego@users.noreply.github.com>
2015-11-07 22:56:34 -0500

    Create gen-certs.sh

---
+++ gen-certs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Generate openssl cert&key for use with sslnchat
+#
+#
+#
+FILENAME=server
+ openssl genrsa -out $FILENAME.key 1024 
+ openssl req -new -key $FILENAME.key -x509 -days 3653 -out $FILENAME.crt 
+ chmod 600 $FILENAME.key 
+ chmod 600 $FILENAME.crt
+echo "Done. Edit server & client script to include new certificates."
