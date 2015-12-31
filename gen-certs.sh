bf7bcc7ce8d50f6b6bab26016ba455712906b80f
Darkerego <darkerego@users.noreply.github.com>
2015-12-31 00:03:22 -0500

    Update gen-certs.sh

--- gen-certs.sh
+++ gen-certs.sh
@@ -6,6 +6,14 @@
 FILENAME=server
  openssl genrsa -out $FILENAME.key 1024 
  openssl req -new -key $FILENAME.key -x509 -days 3653 -out $FILENAME.crt 
+  cat $FILENAME.key $FILENAME.crt >$FILENAME.pem 
  chmod 600 $FILENAME.key 
  chmod 600 $FILENAME.crt
+ FILENAME=client
+ openssl genrsa -out $FILENAME.key 1024 
+ openssl req -new -key $FILENAME.key -x509 -days 3653 -out $FILENAME.crt 
+  cat $FILENAME.key $FILENAME.crt >$FILENAME.pem 
+ chmod 600 $FILENAME.key 
+ chmod 600 $FILENAME.crt
+ 
 echo "Done. Edit server & client script to include new certificates."
